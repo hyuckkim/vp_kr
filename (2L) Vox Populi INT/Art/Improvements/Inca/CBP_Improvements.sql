35a52f2ba838fa366f4add42cac1d62023c94c90
hyuckkim <monkeyhyuck@naver.com>
2022-11-10 19:56:44 +0900

    잉카 텍스트 위치 수정!

--- (2L) Vox Populi INT/Art/Improvements/Inca/CBP_Improvements.sql
+++
@@ -1,4 +0,0 @@
--- Improvements
---------------------------------------------------------------------------------------------------------------------------
-INSERT INTO Language_ko_KR (Tag, Text)
-	VALUES ('TXT_KEY_IMPROVEMENT_JFD_MACHU_PICCHU', '산악 도시');
