37d7dcfe9f71599248ab2cc93d8b49a6126ce260
hyuckkim <monkeyhyuck@naver.com>
2023-08-23 07:58:22 +0900

    (5-35) Buff Mongolia UU and UB

--- (2L) Vox Populi INT/Balance Changes/Text/ko_KR/CivText.sql
+++ (2L) Vox Populi INT/Balance Changes/Text/ko_KR/CivText.sql
@@ -763,7 +763,7 @@ WHERE Tag = 'TXT_KEY_UNIT_MAYAN_ATLATLIST_STRATEGY';
 -- Mongols
 --------------------
 UPDATE Language_ko_KR
-SET Text = '원거리 기마 유닛이 [COLOR_POSITIVE_TEXT]공격할 때[ENDCOLOR] 측면 공격으로 인한 이득을 얻습니다. [ICON_CITY_STATE]도시 국가가 바치는 공물의 산출량이 [COLOR_POSITIVE_TEXT]100%[ENDCOLOR] 증가합니다.'
+SET Text = '원거리 기마 유닛이 추가로 공격할 수 있습니다. [ICON_CITY_STATE]도시 국가가 바치는 공물의 산출량이 [COLOR_POSITIVE_TEXT]100%[ENDCOLOR] 증가합니다.'
 WHERE Tag = 'TXT_KEY_TRAIT_TERROR';
 
 UPDATE Language_ko_KR
